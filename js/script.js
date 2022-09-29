b4788daa290bc91b1c724f49c22189dc0629e88f
David Stevenoot <d.stevenoot@reezocar.com>
2022-09-29 09:24:40 +0200

    version 02

--- js/script.js
+++ js/script.js
@@ -1,9 +1,33 @@
 // navbar
+// toggle hamburger
+const   hamburger = document.querySelector(".navbar-toggler"),
+subNavbar = document.querySelector(".sub-navbar"),
+overLayer = document.querySelector(".overlayer"),
+hamburgerLogo = document.querySelector(".hamburger-logo");
+
+// if (window.matchMedia("(max-width: 1024px)").matches) {
+    hamburger.addEventListener('click', event => {
+        // déplacer la layout supérieure
+        overLayer.classList.toggle("overlayer--open");
+        // animation hamburger
+        hamburgerLogo.classList.toggle("hamburger-logo--open");
+        // toggle navbar
+        subNavbar.classList.toggle("sub-navbar--open");
+        // fermer la dropdown et basculer la flèche a la fermeture de la navbar
+        // let dropdownOpen = dropdownMenu.classList.contains('dropdown-menu--open') ? true : false;
+        // let arrowOpen = arrow.classList.contains('fa-chevron-down--open') ? true : false;
+        // console.log(dropdownOpen)
+        // if (dropdownOpen == true && arrowOpen == true) {
+            //     dropdownMenu.classList.remove("dropdown-menu--open");
+            //     arrow.classList.remove("fa-chevron-down--open");
+            // }
+    });
+// }
+    
 // toggle "nos agences"
 const   dropdown = document.querySelectorAll(".dropdown");
 
 dropdown.forEach(function (e) {
-    console.log(e)
     e.addEventListener('click', event => {
         const   dropdownMenu = event.target.closest(".dropdown").querySelector('.dropdown-menu'),
                 arrow = event.target.closest(".dropdown").querySelector('.fa-chevron-down')
@@ -12,26 +36,4 @@ dropdown.forEach(function (e) {
         // toggle arrow
         arrow.classList.toggle("fa-chevron-down--open");
     })
-});
-        
-// toggle hamburger
-const   hamburger = document.querySelector(".navbar-toggler"),
-        subNavbar = document.querySelector(".sub-navbar"),
-        overLayer = document.querySelector(".overlayer"),
-        hamburgerLogo = document.querySelector(".hamburger-logo");
-
-hamburger.addEventListener('click', event => {
-    // déplacer la layout supérieure
-    overLayer.classList.toggle("overlayer--open");
-    // animation hamburger
-    hamburgerLogo.classList.toggle("hamburger-logo--open");
-    // toggle navbar
-    subNavbar.classList.toggle("sub-navbar--open");
-    // fermer la dropdown et basculer la flèche a la fermeture de la navbar
-    let dropdownOpen = dropdownMenu.classList.contains('dropdown-menu--open') ? true : false;
-    let arrowOpen = arrow.classList.contains('fa-chevron-down--open') ? true : false;
-    if (dropdownOpen == true && arrowOpen == true) {
-        dropdownMenu.classList.remove("dropdown-menu--open");
-        arrow.classList.remove("fa-chevron-down--open");
-    }
 });
