1f66ce966007c83425931924fd43212bdba9bc27
David Stevenoot <d.stevenoot@reezocar.com>
2022-09-29 17:09:09 +0200

    version 5.3

--- js/script.js
+++ js/script.js
@@ -37,11 +37,3 @@ dropdown.forEach(function (e) {
         arrow.classList.toggle("fa-chevron-down--open");
     })
 });
-
-// Selecting the iframe element
-var iframe = document.querySelector("iframe");
-
-// Adjusting the iframe height onload event
-iframe.onload = function(){
-    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-}
