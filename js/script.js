756b597f6d9b38312f3b711c47f5a979a6678061
David Stevenoot <d.stevenoot@reezocar.com>
2022-10-07 15:25:29 +0200

    version 6.4

--- js/script.js
+++ js/script.js
@@ -37,3 +37,34 @@ dropdown.forEach(function (e) {
         arrow.classList.toggle("fa-chevron-down--open");
     })
 });
+
+// lazy load youtube
+( function() {
+
+	var youtube = document.querySelectorAll( ".video-occasion" );
+	
+	for (var i = 0; i < youtube.length; i++) {
+		
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+		
+		var image = new Image();
+				image.src = source;
+				image.addEventListener( "load", function() {
+					youtube[ i ].appendChild( image );
+				}( i ) );
+		
+				youtube[i].addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+
+					    iframe.setAttribute( "allow", "autoplay" );
+							iframe.setAttribute( "frameborder", "0" );
+							iframe.setAttribute( "allowfullscreen", "" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+							this.innerHTML = "";
+							this.appendChild( iframe );
+				} );	
+	};
+	
+} )();
